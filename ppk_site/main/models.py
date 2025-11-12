b97963830653823662daa84c1c916c86d05d7b35
Tigelin <64196924+Bronzobor0d@users.noreply.github.com>
2025-11-12 11:09:08 +1000

    Создание шаблонной страницы и первой модели.

--- ppk_site/main/models.py
+++ ppk_site/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Specialty(models.Model):
+    title = models.CharField('Название', max_length=100)
+    code = models.CharField('Код', max_length=20)
+    qualification = models.CharField('Квалификация', max_length=100)
+    description = models.TextField('Описание')
+    form_of_education = models.CharField('Форма обучения', max_length=20)
+    education_level = models.CharField('Уровень образования', max_length=20)
+    period_of_study = models.CharField('Срок обучения', max_length=20)
+
+    def __str__(self):
+        return self.title
