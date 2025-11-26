f0fa268959c4e73eb8042a51f1197cf054c2650d
Tigelin <151899316+Tigelin@users.noreply.github.com>
2025-11-26 14:06:21 +1000

    Добавлены страницы и навигация связанные с преподавателями.

--- ppk_site/main/urls.py
+++ ppk_site/main/urls.py
@@ -6,7 +6,9 @@ from django.conf import settings
 urlpatterns = [
     path('', views.index, name='home'),
     path('specialities', views.specialities, name='specialities'),
-    path('<int:pk>', views.SpecialtyDetailView.as_view(), name='specialty-detail')
+    path('speciality/<int:pk>', views.SpecialtyDetailView.as_view(), name='specialty-detail'),
+    path('teachers', views.teachers, name='teachers'),
+    path('teacher/<int:pk>', views.TeacherDetailView.as_view(), name='teacher-detail')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ppk_site/main/views.py
+++ ppk_site/main/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Specialty
+from .models import Specialty, Teacher
 from django.views.generic import DetailView
 
 
 def index(request):
     specialities = Specialty.objects.all()[:3]
-    return render(request, 'main/index.html', {'title': 'Главная страница', 'specialities': specialities})
+    teachers = Teacher.objects.all()[:3]
+    return render(request, 'main/index.html', {'title': 'Главная страница', 'specialities': specialities, 'teachers': teachers})
 
 def specialities(request):
     specialities = Specialty.objects.all()
     return render(request, 'main/specialties.html', {'title': 'Специальности', 'specialities': specialities})
 
+def teachers(request):
+    teachers = Teacher.objects.all()
+    return render(request, 'main/teachers.html', {'title': 'Преподаватели', 'teachers': teachers})
 
 class SpecialtyDetailView(DetailView):
     model = Specialty
     template_name = 'main/specialty_detail.html'
     context_object_name = 'specialty'
     extra_context = {'specialities': Specialty.objects.all()}
+
+class TeacherDetailView(DetailView):
+    model = Teacher
+    template_name = 'main/teacher_detail.html'
+    context_object_name = 'teacher'
+    extra_context = {'teachers': Teacher.objects.all()}
